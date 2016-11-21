0ad49c00ff708766e03e245a988dfec4069dd288
nietrwalyenol <nietrwaly.enol@gmail.com>
2016-11-21 20:18:51 +0100

    update

---
+++ src/main/java/com/project/domain/Badanie.java
@@ -0,0 +1,31 @@
+package com.project.domain;
+
+public class Badanie {
+
+	private int id;
+	private String wynik;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getWynik() {
+		return wynik;
+	}
+	public void setWynik(String wynik) {
+		this.wynik = wynik;
+	}
+	public Badanie(int id, String wynik) {
+		this.id = id;
+		this.wynik = wynik;
+	}
+	public Badanie(String wynik) {
+		super();
+		this.wynik = wynik;
+	}
+	public Badanie() {
+		
+	}
+}
